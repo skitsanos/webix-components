774af5981c2333b3ea7b6d25644cd75324d2d23a
Skitsanos <info@skitsanos.com>
2019-09-02 22:08:18 +0300

    Create webix_gravatar.js

---
+++ webix_gravatar.js
@@ -0,0 +1,47 @@
+webix.protoUI({
+    name: 'gravatar',
+    defaults: {
+        width: 40,
+        height: 40,
+        email: undefined,
+        round: false
+    },
+
+    $init: function (config)
+    {
+        this.$ready.push(() =>
+        {
+            const hash = CryptoJS.MD5(this.config.email);
+            let style = '';
+            if (this.config.round)
+            {
+                style = 'border-radius: 50%';
+            }
+            this.$view.innerHTML = `<img src="https://www.gravatar.com/avatar/${hash}?s=${this.config.height-2}" style="${style}"/>`;
+
+            console.log(this.config);
+        });
+
+        this.$view.className += ' webix_gravatar';
+    },
+
+    email_setter: function (value)
+    {
+        return value;
+    },
+
+    round_setter: function (value)
+    {
+        return value;
+    },
+
+    $setSize: function (x, y)
+    {
+        console.log(x,y)
+        if (webix.ui.view.prototype.$setSize.call(this, x, y))
+        {
+            const src = this.$view.firstChild.getAttribute('src').split('?')[0];
+            this.$view.firstChild.setAttribute('src', `${src}?s=${x}`);
+        }
+    }
+}, webix.ui.view);
